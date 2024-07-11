c0c8163c0e4dbc523ad11cf7a903845d041c5bbb
kooriookami <bingshuanglingluo@163.com>
2024-07-11 19:37:50 +0800

    update

--- packages/index.ts
+++ packages/index.ts
@@ -1,2 +1,3 @@
 export * from '@element-plus-leafer/components';
 export * from '@element-plus-leafer/constants';
+export * from '@element-plus-leafer/utils';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { BuildOptions, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import copy from 'rollup-plugin-copy';
 import jsonfile from 'jsonfile';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 function editPackageJson() {
   return {
@@ -11,8 +12,18 @@ function editPackageJson() {
       const file = 'dist/package.json';
       jsonfile.readFile(file, (err, obj) => {
         if (!err) {
-          obj.main = 'umd/index.js';
-          obj.module = 'es/index.mjs';
+          Object.assign(obj, {
+            main: 'umd/index.js',
+            module: 'es/index.mjs',
+            types: 'types/index.d.ts',
+            exports: {
+              '.': {
+                'types': './types/index.d.ts',
+                'import': './es/index.mjs',
+                'require': './umd/index.js',
+              },
+            },
+          });
           jsonfile.writeFile(file, obj, { spaces: 2, EOL: '\r\n' });
         }
       });
@@ -20,43 +31,59 @@ function editPackageJson() {
   };
 }
 
-const buildLib = {
+const buildLib: BuildOptions = {
   lib: {
-    entry: path.resolve(__dirname, 'packages/element-plus-leafer'),
+    entry: path.resolve(__dirname, 'packages'),
     name: 'ElementPlusLeafer',
-    fileName: (format: string) => {
-      if (format === 'es') {
-        return `${format}/index.mjs`;
-      }
-      return `${format}/index.js`;
-    },
   },
   rollupOptions: {
     // 确保外部化处理那些你不想打包进库的依赖
-    external: ['leafer-ui', '@leafer-in/flow', '@leafer-in/state'],
-    output: {
-      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-      globals: {
-        'leafer-ui': 'LeaferUI',
-        '@leafer-in/flow': 'LeaferIN.flow',
-        '@leafer-in/state': 'LeaferIN.state',
+    external: ['leafer-ui', '@leafer-in/flow', '@leafer-in/state', '@ctrl/tinycolor'],
+    output: [
+      {
+        format: 'es',
+        preserveModules: true,
+        preserveModulesRoot: 'packages',
+        entryFileNames: 'es/[name].mjs',
       },
-    },
+      {
+        format: 'cjs',
+        preserveModules: true,
+        preserveModulesRoot: 'packages',
+        entryFileNames: 'lib/[name].cjs',
+      },
+      {
+        format: 'umd',
+        name: 'ElementPlusLeafer',
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          'leafer-ui': 'LeaferUI',
+          '@leafer-in/flow': 'LeaferIN.flow',
+          '@leafer-in/state': 'LeaferIN.state',
+        },
+        entryFileNames: 'dist/[name].js',
+      },
+    ],
     plugins: [
+      dts({
+        tsconfigPath: 'tsconfig.lib.json',
+      }),
       copy({
         targets: [
+          { src: './node_modules/.tmp/types/*', dest: 'dist/types' },
           { src: 'LICENSE', dest: 'dist' },
           { src: 'README.md', dest: 'dist' },
-          { src: 'packages/element-plus-leafer/package.json', dest: 'dist' },
+          { src: 'packages/package.json', dest: 'dist' },
         ],
         hook: 'writeBundle',
       }),
       editPackageJson(),
     ],
   },
+  sourcemap: true,
 };
 
-const buildWebsite = {
+const buildWebsite: BuildOptions = {
   outDir: 'docs',
 };
 
@@ -70,6 +97,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@element-plus-leafer': path.resolve(__dirname, 'packages'),
     },
   },
   build: process.argv.includes('lib') ? buildLib : buildWebsite,
