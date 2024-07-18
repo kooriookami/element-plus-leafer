18c16ce41fb77a589538b85702574af847b7ad7f
kooriookami <bingshuanglingluo@163.com>
2024-07-18 23:21:58 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,10 @@ import path from 'path';
 import dts from 'vite-plugin-dts';
 import packageJson from './packages/package.json';
 
-const getExternal = () => {
-  const { dependencies } = packageJson;
-  return Object.keys(dependencies);
-};
-
-// https://vitejs.dev/config/
-export default defineConfig({
+const baseConfig = defineConfig({
   base: './',
   plugins: [
     vue(),
-    dts({
-      tsconfigPath: 'tsconfig.lib.json',
-      outDir: 'dist/types',
-    }),
   ],
   server: {
     host: '0.0.0.0',
@@ -28,6 +18,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+});
+
+const buildFullConfig = defineConfig({
+  ...baseConfig,
+  plugins: [
+    dts({
+      tsconfigPath: 'tsconfig.lib.json',
+      outDir: 'dist/types',
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'packages'),
@@ -35,22 +35,11 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: getExternal(),
+      external: ['leafer-ui', '@leafer-in/flow', '@leafer-in/state'],
       output: [
         {
-          format: 'es',
-          preserveModules: true,
-          preserveModulesRoot: 'packages',
-          entryFileNames: 'es/[name].mjs',
-        },
-        {
-          format: 'cjs',
-          preserveModules: true,
-          preserveModulesRoot: 'packages',
-          entryFileNames: 'lib/[name].cjs',
-        },
-        {
           format: 'umd',
+          dir: 'dist/dist',
           name: 'ElementPlusLeafer',
           // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
           globals: {
@@ -58,7 +47,7 @@ export default defineConfig({
             '@leafer-in/flow': 'LeaferIN.flow',
             '@leafer-in/state': 'LeaferIN.state',
           },
-          entryFileNames: 'dist/[name].js',
+          entryFileNames: '[name].js',
         },
       ],
       plugins: [
@@ -75,3 +64,43 @@ export default defineConfig({
     sourcemap: true,
   },
 });
+
+const buildLibConfig = defineConfig({
+  ...baseConfig,
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'packages'),
+      name: 'ElementPlusLeafer',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: Object.keys(packageJson.dependencies),
+      output: [
+        {
+          format: 'es',
+          dir: 'dist/es',
+          preserveModules: true,
+          preserveModulesRoot: 'packages',
+          entryFileNames: '[name].mjs',
+        },
+        {
+          format: 'cjs',
+          dir: 'dist/lib',
+          preserveModules: true,
+          preserveModulesRoot: 'packages',
+          entryFileNames: '[name].cjs',
+        },
+      ],
+    },
+    sourcemap: true,
+  },
+});
+
+let config = baseConfig;
+if (process.argv.includes('full')) {
+  config = buildFullConfig;
+} else if (process.argv.includes('lib')) {
+  config = buildLibConfig;
+}
+
+export default config;
